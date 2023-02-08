24d1d413870af986ae84ee82b82f76711c3440ff
Jose Lumbantobing <jolbntobing2@gmail.com>
2023-02-08 14:30:32 +0700

    Finish the programs

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// https://v2.jokeapi.dev/joke/Any?blacklistFlags=religious,racist,sexist&type=single
+const jokes = document.querySelector('.joke');
+const button = document.querySelector('.btn');
+
+const displayJokes = {
+    updateJoke: function() {
+        fetch('https://v2.jokeapi.dev/joke/Any?blacklistFlags=religious,racist,sexist&type=single')
+            .then(response => response.json())
+            .then(data => {
+                jokes.innerText = (data.joke);
+            });
+    }
+}
+
+button.addEventListener('click', function() {
+    displayJokes.updateJoke();
+});
