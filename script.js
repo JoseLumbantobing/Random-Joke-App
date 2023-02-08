d4cf67228b7dfa6ad366a711f83cca3565feee85
Jose Lumbantobing <jolbntobing2@gmail.com>
2023-02-08 15:34:12 +0700

    Improved the program for swapping jokes

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ const button = document.querySelector('.btn');
 
 const displayJokes = {
     updateJoke: function() {
+        jokes.classList.remove('swap');
         fetch('https://v2.jokeapi.dev/joke/Any?blacklistFlags=religious,racist,sexist&type=single')
             .then(response => response.json())
             .then(data => {
                 jokes.innerText = (data.joke);
+                jokes.classList.add('swap');
             });
     }
 }
